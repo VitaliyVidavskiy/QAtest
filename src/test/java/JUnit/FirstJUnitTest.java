e2254883c8522367296783d72f636b0facd83a3c
Vitaliy <mozartaartmo@gmail.com>
2022-10-03 22:40:48 +0300

    added FirstJUnitTest modifed build.gradle

---
+++ src/test/java/JUnit/FirstJUnitTest.java
@@ -0,0 +1,38 @@
+package junit;
+
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.*;
+
+public class FirstJUnitTest {
+    @BeforeAll
+    static void beforeall() {
+        System.out.println("This test will do before all test");
+    }
+
+    @BeforeEach
+    void before() {
+        System.out.println("    This test will do before test");
+    }
+
+    @AfterEach
+    void after() {
+        System.out.println("    This test will do after test");
+    }
+
+    @AfterAll
+    static void afterall() {
+        System.out.println("This test will do after all test");
+    }
+
+    @Test
+    void textTest() {
+        System.out.println("        The first one");
+    }
+
+    @Test
+    void videoTest() {
+        System.out.println("        The second one");
+    }
+}
+
+
