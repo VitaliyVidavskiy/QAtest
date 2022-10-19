af2c9dc4bd3311f23873c047a93497a3a6c69fa5
Vitaliy <mozartaartmo@gmail.com>
2022-10-19 11:41:50 +0300

    modified NewFormTest

--- src/test/java/tests/NewFormTest.java
+++ src/test/java/tests/NewFormTest.java
@@ -1,10 +1,8 @@
 package tests;
 
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 
 import java.io.File;
 
@@ -19,7 +17,7 @@ public class NewFormTest {
     @BeforeAll
     static void beforeall() {
         Configuration.baseUrl="https://demoqa.com";
-        Configuration.browserSize="800x600";
+        Configuration.browserSize="1020x800";
     }
 
     @Test
@@ -35,17 +33,20 @@ public class NewFormTest {
         $(".react-datepicker__month-select").selectOption(3);
         $(".react-datepicker__year-select").selectOption("1993");
         $(by("aria-label", "Choose Saturday, April 24th, 1993")).click();
-        //$(".subjects-auto-complete__input").setValue("Ma").pressEnter();
-
+        $(".subjects-auto-complete__input").click();
+        $("#subjectsInput").setValue("Math").pressEnter();
         $(by("for","hobbies-checkbox-2")).click();
         $(by("for","hobbies-checkbox-3")).click();
         $("#uploadPicture").uploadFile(new File("src/test/java/resources/image.png"));
         $("#currentAddress").setValue("some adress");
-        //$("#state").scrollTo().click();
-        //$(byText("NCR")).click();
-        //$("#city").click();
-        //$(byText("Delhi")).click();
-        //$("#submit").click();
-    }
+        $("#state").scrollTo().click();
+        $("#stateCity-wrapper").$(byText("NCR")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Noida")).click();
+        $("#submit").click();
 
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive").$(byText("Student Name"))
+                .parent().shouldHave(text("viktor jonson"));
+       }
 }
